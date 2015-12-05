b109b75dc8c0e5e1179ce105b360fb5820e01ae6
Sushant Bhattarai <sbhattarai@localhost.localdomain>
2015-12-05 10:47:02 -0500

    Added Random

--- 2048.py
+++ 2048.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import string
+import random 
+
 
 word = "new"
 i = 0
@@ -7,6 +9,7 @@ j = 0
 status = "new"
 direction = "hello"
 hasMoved = False
+numberOfInts = 0
 
 value = [[0 for x in range(4)] for x in range(4)]
 hasMerged = [[False for x in range(4)] for x in range(4)]
@@ -42,7 +45,7 @@ def shift(direction):
 	j = 1
 	#left
 	hasMoved = False 
-	if ((direction == "a") || (direction == "A"))
+	if ((direction == "a") or (direction == "A")):
 		while i <= 3:
 			while j <= 3:
 				if value[i][j] != 0:
@@ -60,9 +63,30 @@ def shift(direction):
 	
 	return
 
+def rand(numberOfInts):
+	rand = 0
+	randomRow = 0
+	randomCol = 0
+	counter = numberOfInts
+	
+	while counter:
+		randomRow = int(random.random() * 4)
+		randomCol = int(random.random() * 4)
+		rand = int(random.random() * 2) *2 + 2
+		if(value[randomRow][randomCol] == 0):
+			value[randomRow][randomCol] = rand
+			counter = counter - 1
+		#else if (gridFull())
+		#	counter = counter - 1
+		print randomRow, randomCol, rand, "hello"
+		counter = counter - 1
+	return
+
 while True:
 		init_grid(status)
 		findDirection()
+		print "entering random"
+		rand(2)
 		while i <= 3:
 			while j <= 3:
 				print value[i][j], " ",
@@ -70,6 +94,7 @@ while True:
 			print "\n"
 			j = 0
 			i = i + 1
+			
 			
 		break
 	
